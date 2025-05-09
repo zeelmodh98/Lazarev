8fe575bdfb3d970bc48d03d7c3dfc94ed240668d
Zeel <zeelmodh98@gmail.com>
2025-05-09 14:26:59 +0530

    commit kiya

--- script.js
+++ script.js
@@ -40,3 +40,48 @@ function navAnimation() {
     })
 }
 navAnimation();
+var rightelem = document.querySelectorAll(".right-elem")
+rightelem.forEach(function(elem) {
+    elem.addEventListener("mouseenter", function() {
+        gsap.to(elem.childNodes[3], {
+            opacity: 1,
+            scale: 1
+        })
+
+    })
+    elem.addEventListener("mouseleave", function() {
+        gsap.to(elem.childNodes[3], {
+            opacity: 0,
+            scale: 0
+        })
+    })
+    elem.addEventListener("mousemove", function(dets) {
+        gsap.to(elem.childNodes[3], {
+            x: dets.x - elem.getBoundingClientRect().x - 50,
+            y: dets.y - elem.getBoundingClientRect().y - 190
+        })
+    })
+})
+
+function page3VideoAnimation() {
+    var page3Center = document.querySelector(".page3-center")
+    var video = document.querySelector("#page3 video")
+
+    page3Center.addEventListener("click", function() {
+        video.play()
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius: 0
+        })
+    })
+    video.addEventListener("click", function() {
+        video.pause()
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity: 0,
+            borderRadius: "30px"
+        })
+    })
+}
+page3VideoAnimation();
